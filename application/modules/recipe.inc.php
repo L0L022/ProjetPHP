a4e8834354847b7314e1a56a34ef0e09a8386174
napoleon789789 <laurentdoiteau@free.fr>
2017-11-29 11:57:46 +0100

    ajout des recettes dans les categories

--- application/modules/recipe.inc.php
+++ application/modules/recipe.inc.php
@@ -1,11 +1,26 @@
 <?php
 
-if (is_numeric($_GET['idc']))
-{
- $data['idc'] = $_GET['idc'];
+if (is_numeric($_GET['idc'])) {
+    $data['idc'] = $_GET['idc'];
+
+    $entree = array(array("id" => 5,"illustration" => "starter.jpg", "name" => "Salade", "description" => "Pour commencer avec le sourire !"),
+    array("id" => 1, "illustration" => "starter.jpg", "name" => "Fromage & salade", "description" => "Pour commencer avec le sourire !"),
+    array("id" => 0, "illustration" => "starter.jpg", "name" => "Cake olive", "description" => "Pour commencer avec le sourire !"));
+
+    $plat = array(array("id" => 6,"illustration" => "main_course.jpg", "name" => "Ratatouille", "description" => "Pour commencer avec le sourire !"),
+    array("id" => 1, "illustration" => "main_course.jpg", "name" => "Frite & steak", "description" => "Pour commencer avec le sourire !"),
+    array("id" => 0, "illustration" => "main_course.jpg", "name" => "Pizza", "description" => "Pour commencer avec le sourire !"));
+
+    $dessert = array(array("id" => 7,"illustration" => "dessert.jpg", "name" => "Mugcake", "description" => "Pour commencer avec le sourire !"),
+    array("id" => 1, "illustration" => "dessert.jpg", "name" => "Crème aux oeufs", "description" => "Pour commencer avec le sourire !"),
+    array("id" => 0, "illustration" => "dessert.jpg", "name" => "Chococo", "description" => "Pour commencer avec le sourire !"));
+
+    $recipesl = array(0 => $entree,1 => $plat,2 => $dessert);
+
+    $data['recipesl'] = $recipesl;
 } else {
- $categories = array(array("id" => 0, "illustration" => "starter.jpg", "name" => "Entrées", "description" => "Pour commencer avec le sourire !")
+    $categories = array(array("id" => 0, "illustration" => "starter.jpg", "name" => "Entrées", "description" => "Pour commencer avec le sourire !")
  , array("id" => 1, "illustration" => "main_course.jpg", "name" => "Plats", "description" => "Pour avoir le ventre bien plein !")
  , array("id" => 2, "illustration" => "dessert.jpg", "name" => "Desserts", "description" => "Pour finir en beauté !"));
- $data['categories'] = $categories;
+    $data['categories'] = $categories;
 }
--- application/views/modules/recipe.tpl
+++ application/views/modules/recipe.tpl
@@ -1,7 +1,24 @@
 {extends file="../layout.tpl"} {block name="body"}
 
 {if isset($idc)}
- <p>Category Id : {$idc}</p>
+
+ <div class="ui divided link items">
+   {foreach $recipesl.$idc as $c}
+   <a class="item" href="?page=recipe&idr={$c.id}">
+    <div class="ui small rounded image">
+     <img src="media/categories/{$c.illustration}">
+    </div>
+    <div class="middle aligned content">
+     <div class="header">{$c.name}</div>
+     <div class="description">
+      <p>{$c.description}</p>
+     </div>
+    </div>
+   </a>
+   {/foreach}
+  </div>
+
+
 {else}
  <div class="ui divided link items">
   {foreach $categories as $c}
